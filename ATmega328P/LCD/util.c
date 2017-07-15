a22c1e9fdd67f438c9ed9c88fe0219f40a2a9a58
Insoo Kim <insoo223@users.noreply.github.com>
2017-07-15 12:36:46 +0900

    Work & Sleep 0.2 uA 초절전 모드 실현!
    LCD 데이터 핀 네개와 Enable핀에 대해서 turn off 한 후, 핀을 input mode로 바꿀 때 비로소 엄청난 잠을 자게 된다. 동작 후 잠자는 동안 전류는 고작 0.2 micro ampere 즉 200 nano ampere 뿐!

--- ATmega328P/LCD/util.c
+++ ATmega328P/LCD/util.c
@@ -221,8 +221,18 @@ void makePDasOutput()
 
 //to remove power usage by LCD (added by Insoo: Jul 12, 2017)
 // Deep Power Save method in circuit
+//As of July 15, 2017(Sat) pm12:30, 
+//	finally achieved sleep current of 0.2 uA or 200 nA 
 void makePDasInput()
 {
+
+	// turn off four data lines 
+	// following four lines added by Insoo on Jul 15, 2017
+	lcd_D7_port &= ~_BV(lcd_D7_bit);
+	lcd_D6_port &= ~_BV(lcd_D6_bit);
+	lcd_D5_port &= ~_BV(lcd_D5_bit);
+	lcd_D4_port &= ~_BV(lcd_D4_bit);
+
 	// configure the microprocessor pins for the data lines
 	// 4 data lines - output, but make it input
 	lcd_D7_ddr &= ~_BV(lcd_D7_bit);
@@ -230,13 +240,11 @@ void makePDasInput()
 	lcd_D5_ddr &= ~_BV(lcd_D5_bit);
 	lcd_D4_ddr &= ~_BV(lcd_D4_bit);
 
+	// turn off backlight cathode(GND)
+	// following one line added by Insoo on Jul 15, 2017
+	lcd_Backlight_port = _BV(lcd_Backlight_bit);
 	// LCD backlight cathode pin (K) - output, but make it input
 	lcd_Backlight_ddr &= ~_BV(lcd_Backlight_bit);
-
-	// turn off LCD 
-	lcd_VDD_port &= ~_BV(lcd_VDD_bit);
-	// then, LCD VSS pin (Power Supply) - output, but make it input
-	lcd_VDD_ddr &= ~_BV(lcd_VDD_bit);
 
 	// turn off DS1307
 	DS1307_VCC_port &= ~_BV(DS1307_VCC_bit);
@@ -250,14 +258,17 @@ void makePDasInput()
 	DDRB &= ~_BV(TINUDHT_PIN);
 	PORTB &= ~_BV(TINUDHT_PIN);
 
-	//turn off DS1307 RTC chip power
-	//DHT_VCC_port &= ~_BV(DHT_VCC_bit);
+	//turn off DHT11 chip power
+	DHT_VCC_port &= ~_BV(DHT_VCC_bit);
 	//make DHT_VCC as input, added by Insoo (Jul 14, 2017)
 	DHT_VCC_ddr &= ~_BV(DHT_VCC_bit);
 
 	//Tactile switch - Input
 	//tactile_Switch_ddr &= ~_BV(tactile_Switch_bit);
 
+	// turn off Enable pin
+	// following one line added by Insoo on Jul 15, 2017
+	lcd_E_port &= ~_BV(lcd_E_bit);
 	// configure the microprocessor pins for the control lines
 	// E line - output, but make it input
 	lcd_E_ddr &= ~_BV(lcd_E_bit);
@@ -266,6 +277,11 @@ void makePDasInput()
 	lcd_RS_port &= ~_BV(lcd_RS_bit);
 	// RS line - output, but make it input
 	lcd_RS_ddr &= ~_BV(lcd_RS_bit);
+
+	// turn off LCD power
+	lcd_VDD_port &= ~_BV(lcd_VDD_bit);
+	// then, LCD VSS pin (Power Supply) - output, but make it input
+	lcd_VDD_ddr &= ~_BV(lcd_VDD_bit);
 
 }//makePDasInput
 
